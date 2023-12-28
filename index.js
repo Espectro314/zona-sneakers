d25f586a1b45bdb8b736debc09a8ea72b84c7654
Espectro314 <143041206+Espectro314@users.noreply.github.com>
2023-12-28 02:43:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const scrapeWebsite = require('./scraper');
 const scrapeWebsiteDesc = require('./scraperDetalles');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
